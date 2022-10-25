48804d94add971fb80e08c08fdc9f4191bc943f0
spences10 <spences10apps@gmail.com>
2022-10-25 21:31:47 +0100

    refactor: :recycle: add query string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,17 +17,17 @@
 	from Geoff Rich to learn more.
 </p>
 
-<input type="text" bind:value={title}>
-<input type="text" bind:value={author}>
-<input type="text" bind:value={website}>
-<input type="text" bind:value={img}>
+<p>You can also check play around with the vriables here.</p>
+
+<input type="text" bind:value={title} />
+<input type="text" bind:value={author} />
+<input type="text" bind:value={website} />
+<input type="text" bind:value={img} />
 
 <p>
-	You can also check out the <a
-		href={`/og?title=${title}&author=${author}&website=${website}&img=${img}`}
-		target="_blank"
-		rel="noreferrer">demo</a
-	>.
+	URL query string: <code>
+		{`/og?title=${title}&author=${author}&website=${website}&img=${img}`}
+	</code>
 </p>
 
 <img
